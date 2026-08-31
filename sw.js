af1c7e28878cfcab5b2f34aa55471eb6d65d4b86
Ryan Madrid <Tancouch@users.noreply.github.com>
2026-08-31 11:40:06 -0700

    Add down payment as a rate lever and name LLPAs as the mechanism

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Bison Field Manual service worker, build ae31a9c605 */
-var CACHE = "bison-manual-ae31a9c605";
+/* Bison Field Manual service worker, build 6bb27a4073 */
+var CACHE = "bison-manual-6bb27a4073";
 var CORE = ["./", "./index.html", "./manifest.webmanifest",
             "./icons/icon-192.png", "./icons/icon-512.png",
             "./icons/apple-touch-icon.png", "./icons/favicon-32.png"];
